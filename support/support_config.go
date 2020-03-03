be72be6ddaed91f3e105f8e01fdd8c284557f6c4
aumujun <aumujun@gmail.com>
2020-03-04 00:30:01 +0800

    add: ReadConfigFile impl

--- support/support_config.go
+++ support/support_config.go
@@ -1,8 +1,17 @@
 package support
 
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/0x1un/itopmid/iface"
+	json "github.com/json-iterator/go"
+)
+
 type ItopMidConfig struct {
 	ItopUrl       string `json:"itopurl"`
 	ItopUsername  string `json:"itopusername"`
+	ItopPasswd    string `json:"itoppasswd"`
 	DingAppkey    string `json:"dingappkey"`
 	DingAppsecret string `json:"dingappsecret"`
 	DingApprovID  string `json:"dingapprovid"`
@@ -11,17 +20,20 @@ type ItopMidConfig struct {
 	PostgresUrl   string `json:"postgresurl"`
 }
 
+func Test() {
+	i := &ItopMidConfig{}
+	i.ReadConfigFile("./itopmid.json")
+	fmt.Println(*i)
+}
+
 func (self *ItopMidConfig) Init() {}
 
 func (self *ItopMidConfig) ReadConfigFile(filename string) {
-
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		iface.LOGGER.Error("Failed open file:", err.Error())
+	}
+	if err := json.Unmarshal(content, self); err != nil {
+		iface.LOGGER.Error("Failed to unmarshal json file:", err.Error())
+	}
 }
-
-func (self *ItopMidConfig) GetItopUrl() string {
-	return self.ItopUrl
-}
-
-// func (self *ItopMidConfig)
-// func (self *ItopMidConfig)
-// func (self *ItopMidConfig)
-// func (self *ItopMidConfig)
