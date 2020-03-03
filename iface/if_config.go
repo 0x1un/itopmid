40a170835423b9659804e1a52d0f3d8a9015ef62
0x1un <aumujun@gmail.com>
2020-03-03 17:05:45 +0800

    add some comments

--- iface/if_config.go
+++ iface/if_config.go
@@ -1,12 +1,12 @@
 package iface
 
 type Configer interface {
-	ItopUrl() string       // itop rest api url
-	ItopUsername() string  // itop username
-	DingAppkey() string    // dingtalk appkey
-	DingAppsecret() string // dingtalk appsecret
-	DingApprovID() string  // 钉钉审批单模板列表
-	DingAgentID() string   // 钉钉应用的agentd
-	DingUserID() string    // 钉钉用户的uid
-	PostgresUrl() string   // postgres数据库链接
+	GetItopUrl() string       // itop rest api url
+	GetItopUsername() string  // itop username
+	GetDingAppkey() string    // dingtalk appkey
+	GetDingAppsecret() string // dingtalk appsecret
+	GetDingApprovID() string  // 钉钉审批单模板列表
+	GetDingAgentID() string   // 钉钉应用的agentd
+	GetDingUserID() string    // 钉钉用户的uid
+	GetPostgresUrl() string   // postgres数据库链接
 }
