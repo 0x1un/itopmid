af5b1dafc48e6958a0ed84ad7980cc11b6bc25f4
0x1un <aumujun@gmail.com>
2020-03-04 23:57:40 +0800

    rewrite: Configer interface

--- iface/if_config.go
+++ iface/if_config.go
@@ -1,12 +1,14 @@
 package iface
 
 type Configer interface {
-	GetItopUrl() string       // itop rest api url
-	GetItopUsername() string  // itop username
-	GetDingAppkey() string    // dingtalk appkey
-	GetDingAppsecret() string // dingtalk appsecret
-	GetDingApprovID() string  // 钉钉审批单模板列表
-	GetDingAgentID() string   // 钉钉应用的agentd
-	GetDingUserID() string    // 钉钉用户的uid
-	GetPostgresUrl() string   // postgres数据库链接
+	ReadConfigFile(filename string)
+	GetItopUrl() string
+	GetItopUsername() string
+	GetItopPassword() string
+	GetDingAppkey() string
+	GetDingAppsecret() string
+	GetDingApprovID() string
+	GetDingAgentID() string
+	GetDingUserID() string
+	GetDatabaseURL() string
 }
