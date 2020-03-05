3a57e3cd1bccc0647b5a30b3000b1ed039900d35
aumujun <aumujun@gmail.com>
2020-03-05 21:33:33 +0800

    main test

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
+	"fmt"
 	_ "net/http/pprof"
 
+	"github.com/0x1un/boxes/dingtalk/api"
+	"github.com/0x1un/itopmid/db"
 	"github.com/0x1un/itopmid/iface"
 	"github.com/0x1un/itopmid/support"
-)
-
-// 釘釘應用程序的agentid
-const (
-// iTopLocalURL = `http://localhost:8000/webservices/rest.php?version=1.3`
 )
 
 func init() {
@@ -25,33 +23,30 @@ func init() {
 
 func main() {
 
-	iface.LOGGER.Debug("这特么只是一个测试")
-	iface.LOGGER.Debug("%s", iface.CONFIG.GetDatabaseURL())
-	// mux := http.NewServeMux()
-	// mux.HandleFunc("/custom_debug_path/profile", pprof.Profile)
-	// log.Fatal(http.ListenAndServe(":7777", mux))
-	// request_data, err := NewRestAPIAuthData("admin", "...")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	//
-	// conn, err := db.NewDBConnect()
-	// if err != nil {
-	// 	panic(err)
-	// }
+	request_data, err := NewRestAPIAuthData(iface.CONFIG.GetItopUsername(), iface.CONFIG.GetItopPassword())
+	if err != nil {
+		panic(err)
+	}
+
+	conn, err := db.NewDBConnect()
+	if err != nil {
+		panic(err)
+	}
 
 	// 从itop中获取所有状态为开启的工单
-	// resp := FetcheFromITOP(iTopLocalURL, request_data)
-	// for _, v := range resp.Object {
-	// 	fmt.Println(v.Filed)
-	// 	if err := StoreTicketFromITOP(conn, v.Filed); err != nil {
-	// 		log.Println(err)
-	// 	}
-	// }
+	resp := FetcheFromITOP(iface.CONFIG.GetItopUrl(), request_data)
+	for _, v := range resp.Object {
+		fmt.Println(v.Filed)
+		if err := StoreTicketFromITOP(conn, v.Filed); err != nil {
+			iface.LOGGER.Error(err.Error())
+		}
+	}
 
-	// client := api.NewClient(api.APPKEY, api.APPSECRET)
-	// // 发送来自itop的工单
-	// if err := SendToProv(client, resp); err != nil {
-	// 	panic(err)
-	// }
+	client := api.NewClient(iface.CONFIG.GetDingAppkey(), iface.CONFIG.GetDingAppsecret())
+	client.ProcessReq.DeptId = iface.CONFIG.GetDingDeptID()
+	client.ProcessReq.AgentId = iface.CONFIG.GetDingAgentID()
+	client.ProcessReq.OriginatorUserId = iface.CONFIG.GetDingUserID()
+	client.ProcessReq.ProcessCode = iface.CONFIG.GetDingApprovID()
+	// 发送来自itop的工单至钉钉工单中
+	SendToProv(client, resp)
 }
