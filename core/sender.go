40716281209ea504b309d32ae30b2dbd6dd9e917
aumujun <aumujun@gmail.com>
2020-03-07 23:54:33 +0800

    update send flag when send to dingtalk

--- core/sender.go
+++ core/sender.go
@@ -3,16 +3,44 @@ package core
 import (
 	"github.com/0x1un/boxes/dingtalk/api"
 	"github.com/0x1un/itopmid/iface"
+	"github.com/0x1un/itopmid/support"
 )
 
+const (
+	SEND_SUCC_CODE    = 0
+	SEND_ERR_CODE     = 1
+	DATABASE_ERR_CODE = 2
+)
+
+// 发送到钉钉遇到失败，放入此队列等待重试
+var retryQueue = make(map[string]api.FormValues)
+
 // 这里调用SendProcess批量发送工单
-func SendToProv(c *api.DingTalkClient, resp UserReqResponse) {
-	formValueArray := ConvertUserRequest(resp)
-	for _, v := range *formValueArray {
+func SendToDingtalkProcess(c *api.DingTalkClient, resp support.UserReqResponse) {
+	formComponent := ConvertUserRequest(resp)
+	for k, v := range formComponent {
 		response, err := c.SendProcess(v)
 		if response.ErrCode != 0 || err != nil {
 			iface.LOGGER.Error(response.ErrMsg)
+			retryQueue[k] = v
+			continue
 		}
-		iface.LOGGER.Info("Sent a message succeeded")
+		if _, err = setItopTicketFlag(k); err != nil {
+			iface.LOGGER.Error(err.Error())
+		}
 	}
+}
+
+// 标记itop工单为已发送, 字段 *send*
+func setItopTicketFlag(ref string) (int, error) {
+	h := iface.CONTEXT.GetDB().Begin()
+
+	err := h.Table("itop_ticket").Where("ref=?", ref).Update("send", true).Error
+	if err != nil {
+		// iface.LOGGER.Error("Failed update field: %s", err.Error())
+		h.Rollback()
+		return DATABASE_ERR_CODE, err
+	}
+	h.Commit()
+	return SEND_SUCC_CODE, nil
 }
