8731ec8d1a30ee03a5e4f67c743dc2ba8e0b63ca
aumujun <aumujun@gmail.com>
2020-03-07 23:51:15 +0800

    refactor: fetcher rewrite

--- core/fetcher.go
+++ core/fetcher.go
@@ -3,51 +3,67 @@ package core
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"strings"
 
 	"github.com/0x1un/itopmid/iface"
-	"github.com/jinzhu/gorm"
+	"github.com/0x1un/itopmid/support"
 )
 
-func ListenITOP(url string, data io.Reader) <-chan UserReqResponse {
-
-	return nil
-}
-
 // 返回来自itop的标准门户工单数据
-func FetcheFromITOP(url string, data io.Reader) UserReqResponse {
+func FetcheFromITOP(url string, data io.Reader) {
 	resp, err := request(http.MethodPost, url, data)
 	if err != nil {
 		iface.LOGGER.Panic(err.Error())
 	}
-	t := new(UserReqResponse)
+	t := new(support.UserReqResponse)
 	if err := json.Unmarshal(resp, t); err != nil {
 		iface.LOGGER.Panic(err.Error())
 	}
-	return *t
+	// 将获取的工单数据插入到数据库
+	for _, v := range t.Object {
+		// 如果获取的工单在数据库中不存在，则创建一条新记录
+		ref := v.Filed.Ref
+		if !checkEntry(ref) {
+			if err := insertTicketITOP(v.Filed); err != nil {
+				iface.LOGGER.Error("Got error: %s", err.Error())
+			}
+			iface.LOGGER.Info("ref: %s is inserted", ref)
+		} else {
+			// iface.LOGGER.Error("%s entry is already exists!", ref)
+			continue
+		}
+	}
+	SendToDingtalkProcess(iface.CLIENT, *t)
 }
 
 // 对数据库插入itop工单数据，插入的数据为Fileds中的工单详情
-func StoreTicketFromITOP(conn *gorm.DB, ticket Fileds) (e error) {
-	h := conn.Begin()
+func insertTicketITOP(ticket support.Fileds) error {
+	var e error
+	h := iface.CONTEXT.GetDB().Begin()
 	h = h.Table("itop_ticket")
-	isNotFound := h.Select("ref").Where("ref=?", ticket.Ref).Scan(&struct{ Ref string }{}).RecordNotFound()
-	if isNotFound {
-		e = h.Create(ticket).Error
-		if e != nil {
-			h.Rollback()
-			return e
-		}
-	} else {
-		return errors.New(fmt.Sprintf("错误: 重复插入条目: %s", ticket.Ref))
+	e = h.Create(ticket).Error
+	if e != nil {
+		h.Rollback()
+		return e
 	}
 	h.Commit()
 	return nil
+}
+
+// 判断itop的工单是否已经被获取, 判断的依据是itop中工单唯一编码ref
+// 如果没有 返回false, 反之亦然
+func checkEntry(ref string) bool {
+	h := iface.CONTEXT.GetDB().Begin()
+	h = h.Table("itop_ticket")
+	nf := h.Select("ref").Where("ref=?", ref).Scan(&struct{ Rf string }{}).RecordNotFound()
+	if nf {
+		return false
+	} else {
+		return true
+	}
 }
 
 // 简单封装的http请求
