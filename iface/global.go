8f82cd4ede7e2153b3702331cbf904e2bc3d501f
aumujun <aumujun@gmail.com>
2020-03-07 23:55:56 +0800

    defin: request interface and client

--- iface/global.go
+++ iface/global.go
@@ -1,7 +1,17 @@
 package iface
 
-var LOGGER Logger
+import "github.com/0x1un/boxes/dingtalk/api"
 
-var CONFIG Configer
+var LOGGER Logger // 日志
 
-var CONTEXT ItopMidContext
+var CONFIG Configer // 配置
+
+var CONTEXT ItopMidContext // 上下文，包含数据库，缓存之类的东西
+
+var ITOP_USERNAME string // ITOP username
+
+var ITOP_PASSWORD string // ITOP password
+
+var REQUEST Requester // ITOP 请求的数据，包含UserRequest, Contact, Person 之类的请求数据与响应数据
+
+var CLIENT *api.DingTalkClient
