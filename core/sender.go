3af11defe67f9ab136264620e0f799269a93c310
aumujun <aumujun@gmail.com>
2020-03-08 13:55:09 +0800

    comments: status code

--- core/sender.go
+++ core/sender.go
@@ -6,11 +6,11 @@ import (
 	"github.com/0x1un/itopmid/support"
 )
 
-const (
-	SEND_SUCC_CODE    = 0
-	SEND_ERR_CODE     = 1
-	DATABASE_ERR_CODE = 2
-)
+// const (
+// 	SEND_SUCC_CODE    = 0
+// 	SEND_ERR_CODE     = 1
+// 	DATABASE_ERR_CODE = 2
+// )
 
 // 发送到钉钉遇到失败，放入此队列等待重试
 var retryQueue = make(map[string]api.FormValues)
@@ -25,22 +25,20 @@ func SendToDingtalkProcess(c *api.DingTalkClient, resp support.UserReqResponse) 
 			retryQueue[k] = v
 			continue
 		}
-		if _, err = setItopTicketFlag(k); err != nil {
+		if err := setItopTicketFlag(k); err != nil {
 			iface.LOGGER.Error(err.Error())
 		}
 	}
 }
 
 // 标记itop工单为已发送, 字段 *send*
-func setItopTicketFlag(ref string) (int, error) {
+func setItopTicketFlag(ref string) error {
 	h := iface.CONTEXT.GetDB().Begin()
-
 	err := h.Table("itop_ticket").Where("ref=?", ref).Update("send", true).Error
 	if err != nil {
-		// iface.LOGGER.Error("Failed update field: %s", err.Error())
 		h.Rollback()
-		return DATABASE_ERR_CODE, err
+		return err
 	}
 	h.Commit()
-	return SEND_SUCC_CODE, nil
+	return nil
 }
