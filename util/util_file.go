b1b666a6c02dc98dc8b4a27ff10c77aab124b393
aumujun <aumujun@gmail.com>
2020-03-11 22:29:30 +0800

    impl: GetCurrentPath function

--- util/util_file.go
+++ util/util_file.go
@@ -1,5 +1,14 @@
 package util
 
-func GetCurrentPath(absPath string) string {
-	return absPath
+import "strings"
+
+func GetCurrentPath(fullPath string) string {
+	index1 := strings.LastIndex(fullPath, "/")
+	//maybe windows env
+	index2 := strings.LastIndex(fullPath, "\\")
+	index := index1
+	if index2 > index1 {
+		index = index2
+	}
+	return fullPath[index+1:]
 }
