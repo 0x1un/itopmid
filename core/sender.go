d9df912c673219e02f3210b096f79601da0635be
0x1un <aumujun@gmail.com>
2020-03-12 17:52:49 +0800

    remove some useless codes

--- core/sender.go
+++ core/sender.go
@@ -3,16 +3,9 @@ package core
 import (
 	"fmt"
 
-	"github.com/0x1un/boxes/dingtalk/api"
 	"github.com/0x1un/itopmid/iface"
 	"github.com/0x1un/itopmid/support"
 )
-
-// const (
-// 	SEND_SUCC_CODE    = 0
-// 	SEND_ERR_CODE     = 1
-// 	DATABASE_ERR_CODE = 2
-// )
 
 func SendSingleTicketToDingtalkProcess(content *support.ResponseContent) error {
 	ref := content.Filed.Ref
@@ -31,30 +24,6 @@ func SendSingleTicketToDingtalkProcess(content *support.ResponseContent) error {
 	iface.LOGGER.Info("Sent ticket: *%s* to dingtalk process", ref)
 	// }
 	return nil
-}
-
-// 这里调用SendProcess批量发送工单
-func sendToDingtalkProcess(c *api.DingTalkClient, resp support.UserReqResponse) {
-	formComponent := ConvertBatchUserRequest(resp)
-	for k, v := range formComponent {
-		// 如果查到这个值并没有被发送过的记录，则将其发送
-		if !isSend(k) {
-			response, err := c.SendProcess(v)
-			// 如果发送失败，输出失败原因到日志
-			if response.ErrCode != 0 || err != nil {
-				iface.LOGGER.Error(response.ErrMsg)
-				continue
-			}
-			if err := setItopTicketFlag(k); err != nil {
-				iface.LOGGER.Error(err.Error())
-			}
-			iface.LOGGER.Info("Sent a ticket: %s to dingtalk", k)
-		}
-	}
-}
-
-// 发送单个工单到钉钉, 依然调用钉钉的SendProcess接口（自行封装的)
-func SendSingleTicketToDingtalk() {
 }
 
 // 标记itop工单为已发送, 字段 *send*
