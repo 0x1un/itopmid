6f88f61c9d6be47f8ce02e91b1bc1c4674d3010c
0x1un <aumujun@gmail.com>
2020-03-16 18:05:11 +0800

    dev: fetcher get process status by id

--- core/ding_fetcher.go
+++ core/ding_fetcher.go
@@ -1,5 +1,11 @@
 package core
 
+import (
+	"fmt"
+
+	"github.com/0x1un/itopmid/iface"
+)
+
 const (
 	PROCESS_IS_NEW        = 0
 	PROCESS_IS_RUNNING    = 1
@@ -8,4 +14,11 @@ const (
 )
 
 func GetProcessStatusByID(id string) int {
+	res, err := iface.CLIENT.GetProcessInstanceDetail(id)
+	if err != nil {
+		iface.LOGGER.Error(err.Error())
+		return PROCESS_IS_TERMINATED
+	}
+	fmt.Println(res)
+	return 0
 }
