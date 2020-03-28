71e6a685fc39cc3db3f1eaef35f5003a78dbbd36
aumujun <aumujun@gmail.com>
2020-03-28 22:16:57 +0800

    impl: TicketQueuer interface

--- support/support_queue.go
+++ support/support_queue.go
@@ -1,13 +1,32 @@
 package support
 
-type Queue []ResponseContent
+type Queue []interface{}
+
+type TicketQueue map[string]string
+
+func (self TicketQueue) Set(k, v string) {
+	self[k] = v
+}
+
+func (self TicketQueue) Del(k string) {
+	delete(self, k)
+}
+
+func (self TicketQueue) Self() map[string]string {
+	return self
+}
 
 func (self *Queue) Push(respContent interface{}) {
-	*self = append(*self, (ResponseContent)(respContent.(ResponseContent)))
+	switch respContent.(type) {
+	case string:
+		*self = append(*self, respContent.(string))
+	case ResponseContent:
+		*self = append(*self, respContent.(ResponseContent))
+	}
 }
 
 func (self *Queue) Pop() interface{} {
-	if n := len(*self); n == 0 {
+	if n := self.Len(); n == 0 {
 		return nil
 	} else {
 		v := (*self)[n-1]
@@ -16,6 +35,14 @@ func (self *Queue) Pop() interface{} {
 	}
 }
 
+func (self *Queue) Tail() interface{} {
+	return (*self)[self.Len()-1]
+}
+
 func (self *Queue) Len() int {
 	return len(*self)
+}
+
+func (self *Queue) Self() interface{} {
+	return *self
 }
